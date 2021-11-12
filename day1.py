2df716b26bb68ac48226bf3d6e82ab4ade01ae3c
Simeon-paul Taiwo Emmanuel <65008596+Emmanuel2202@users.noreply.github.com>
2021-11-12 14:14:17 +0100

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,22 @@
+print('Hello world')
+number = 7
+number2 = 12.0
+word = 'Python'
+x = True
+print(number)
+word2 = 'It\'s a good to live long'
+print(word2)
+word3 = "It's a good to live long"
+poem = 'oh i love nature,\nhow i wish the moon is always out,\nthe stars should stay up in the sky '
+print(poem)
+
+statement = '''Hey python is fun and easy
+are you not going to try it out
+oh my dear you will love it
+see you soon
+'''
+print('\n')
+print(statement)# hey i am displaying statement
+
+print('\n')
+print('hello' + ' ' +'world')
---
+++ day2.py
@@ -0,0 +1,4 @@
+price1 = 15000
+price2 = 25000
+price3 = 80000
+print(f'Good day boss, summary of todays sales, i sold 4 pairs of shirts for {price1}, a shoe for {price2} and a suit for {price3}')
